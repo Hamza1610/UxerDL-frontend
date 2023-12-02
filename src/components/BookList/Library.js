470cd7b6328855ded83530098fc5dec02b41f398
Hamza1060 <muhamadhamza162003@gmail.com>
2023-12-01 18:44:45 -0800

    incluide modal and class apis

--- src/components/BookList/Library.js
+++ src/components/BookList/Library.js
@@ -1,30 +1,26 @@
 import { useEffect, useState } from "react";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
-import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Cards from "./Cards";
 import SearchBar from "../SearchBar/SearchBar";
-
+import CustomModal from '../modal';
+import {Books} from '../../apis/Books'
 const Library = () => {
 
-    const [lists, setLists] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [selectStyle, setSelectStyle] = useState({color: 'white', fontWeight: 'bolder'});
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-          }, 2000);
-          console.log('Logging something');
-        // ListsFetch();
-    },[])
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const [key, setKey] = useState('trending');
-
+     
+    const mybook = new Books()
+    console.log(mybook);
     return (
         <Container >
+            <CustomModal close={handleClose} show={show}/>
             <Row>
                 <Col sm={12}>
                     <Row>
@@ -33,26 +29,26 @@ const Library = () => {
                             id="controlled-tab-example"
                             activeKey={key}
                             onSelect={(k) => setKey(k)}
-                            className="mb-3"
-                            variant="tabs"
-                            style={{fontWeight: "bold", color: "white", position: "sticky", top: "0", zIndex: 1}} 
+                            className="mb-2" 
+                            style={{fontWeight: "bold", position: "sticky", top: "0", zIndex: 1}} 
+                            
                         >
                             <Tab
                                 title="Trending books"
-                                eventKey="trending"   
+                                eventKey="trending"
                             >
                                 <SearchBar hint='Search books' />
-                                <Row className="justify-content-center" style={{overflowY: "scroll", height: "86vh"}}>
-                                    <Cards />
+                                <Row className="justify-content-center" style={{overflowY: "scroll", height: "87vh"}}>
+                                    <Cards more={handleShow}/>
                                 </Row>
                             </Tab>
                             <Tab
                                 title="Latest books"
-                                eventKey="latest"    
+                                eventKey="latest"
                             >
                                 <SearchBar hint='Search books' />
-                                <Row className="justify-content-center" style={{overflowY: "scroll", height: "86vh"}}>
-                                    <Cards />
+                                <Row className="justify-content-center" style={{overflowY: "scroll", height: "87vh"}}>
+                                    <Cards more={handleShow}/>
                                 </Row>
                             </Tab>
                             <Tab
@@ -60,19 +56,19 @@ const Library = () => {
                                 eventKey="books"    
                             >
                                 <SearchBar hint='Search books' />
-                                <Row className="justify-content-center" style={{overflowY: "scroll", height: "86vh"}}>
-                                    <Cards />
+                                <Row className="justify-content-center" style={{overflowY: "scroll", height: "87vh"}}>
+                                    <Cards more={handleShow}/>
                                 </Row>
                             </Tab>
 
                             <Tab
                                 title="Personal library"
-                                eventKey="personal"    
+                                eventKey="personal"   
                             >
                                 <SearchBar hint='Search books' />
                                 <Row>
-                                <Row className="justify-content-center" style={{overflowY: "scroll", height: "86vh"}}>
-                                    <Cards />
+                                <Row className="justify-content-center" style={{overflowY: "scroll", height: "87vh"}}>
+                                    <Cards more={handleShow}/>
                                 </Row>
                                 </Row>
                             </Tab>
