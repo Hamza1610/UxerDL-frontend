c9bb7d53e00b699afc7a327e3bf367a390f85ddb
Hamza1060 <muhamadhamza162003@gmail.com>
2023-12-01 18:43:06 -0800

    added auth components

--- src/components/AuthComponents/SignIn.js
+++ src/components/AuthComponents/SignIn.js
@@ -1,3 +1,7 @@
+import {useState} from 'react';
+import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { auth } from './firebase-config'
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
@@ -5,6 +9,22 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
 const SignIn =  () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassowrd] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const user = await signInWithEmailAndPassword(auth, email, password);
+            console.log(user);
+        } catch (error) {
+            console.log(error.message);
+            setError('invalid email or password');
+
+        }
+    }
     return (
         <Container>
             <Row md={4}>
@@ -13,18 +33,9 @@ const SignIn =  () => {
                 </Col>
             </Row>
             <Row className='align-items-center h-75 w-100 justify-content-center' md={4} >
-                <Col sm={2}>
-                    <Form>
-                        <Form.Label style={{fontSize: '1.3em'}}>Sign in with your account: </Form.Label>
-                        <Form.Group >
-                            <Button size='lg' type='submit' className='m-3 '>Google</Button>
-                            <Button size='lg' type='submit' className='m-3'>Facebook</Button>
-                        </Form.Group>
-                    </Form>
-                </Col>
                 <Col >
-                    <Form >
-                        <Form.Label style={{fontSize: '1.3em'}}>Sign in your email: </Form.Label>
+                    <Form  onSubmit={handleSubmit}>
+                        <Form.Label style={{fontSize: '1.3em'}}>Sign in your account: </Form.Label>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control name='email' size='lg' type="email" placeholder="Enter email" className='w-100'/>
@@ -38,6 +49,23 @@ const SignIn =  () => {
                         Submit
                         </Button><br/>
                         <Form.Label>Don't account? <a href='/sign-up'>Sign up</a></Form.Label>
+                    </Form>
+                    <Form>
+                        <hr style={{fontSize: '100px', color: 'white'}}/>
+                        <Form.Group >
+                            <Button
+                                size='lg'
+                                type='submit'
+                                className='m-3'
+                                onClick={signInWithPopup(auth, 'google')}
+                                >Google</Button>
+                            <Button
+                                size='lg'
+                                type='submit'
+                                className='m-3'
+                                onClick={signInWithPopup(auth, 'facebook')}
+                                >Facebook</Button>
+                        </Form.Group>
                     </Form>
                 </Col>
             </Row>
