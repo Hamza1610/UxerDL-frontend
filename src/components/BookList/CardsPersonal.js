4d772498fbd4cea4c88eb3f07b8fd9a6c9666cb8
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-06 03:18:44 -0800

    addad search componentes

--- src/components/BookList/CardsPersonal.js
+++ src/components/BookList/CardsPersonal.js
@@ -1,33 +1,94 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import logo from './logo.png'
-import { useState } from 'react';
 import Books from "../../apis/Books";
 import { useEffect } from 'react';
+import CustomModal from '../modal';
+import { Row } from 'react-bootstrap';
+import SearchBar from '../SearchBar/SearchBar';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+import Preloader from '../preloader';
 
 const Personal = (props) => {
 
-    const handleClick = () => {
-        // returns more function
-        return props.more();
+    const BookClass = new Books();
+    const [booksContent, setBooksContent] = useState([])
+
+    const [show, setShow] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [content, setContent] = useState({})
+    const [searchText, setSearchText] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const data = await  BookClass.get_book(searchText)
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+            setBooksContent(data.items)
+            setSearchText('')
+        }, 3000);
+        console.log('Loading')
+        
+    }
+    
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+    const handleClick = (item) => {
+        // returns more function 
+        setContent(item)
+        console.log(item);  
+        handleShow()
    }
-  const books = [1,2,3,4,5,6,7,8]
+
+   const renderImg = (link) => {
+        try {
+            return (<Card.Img variant="top" src={link.imageLinks.thumbnail} alt='Book Image' />)
+        }
+        catch(error) {
+            console.log(error);
+            return (<Card.Img variant="top" src={link.imageLinks} alt='Book Image' />)
+        }
+
+    }
 
   return (
-        books.map((key, index) => (
-            // map card components
-            <Card style={{ width: '18rem', }} key={key} className='m-2'>
-                <Card.Img variant="top" src={logo} />
-                <Card.Body>
-                    <Card.Title>Title : {books}</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary" onClick={handleClick}>More details</Button>
-                </Card.Body>
-            </Card>     
-        ))
-    )
+    <>
+        <CustomModal close={handleClose} show={show} contents= {content} />
+        <Stack className='mb-2'>
+            <Form onSubmit={handleSubmit} style={{width: '100%',display: 'flex', }}>
+                    <Form.Control
+                        type='search'
+                        className="w-100"
+                        placeholder='Search book'
+                        onChange={(e)=> setSearchText(e.target.value)}
+                    />      
+                    <Button type='submit'>Search</Button>
+                </Form>
+        </Stack>
+        {loading && (<Preloader />)}
+        {
+            booksContent && booksContent.map((item) => (
+                // map card components
+                <Card style={{ width: '18rem', }} key={item.id} className='m-2'>
+                    <Card.Body>
+                        {renderImg(item.volumeInfo)}
+                        <Card.Title><b>Title:</b> {item.volumeInfo.title}</Card.Title>
+                        <Card.Text>
+                            <p><b>Subtitle:</b> {item.volumeInfo.subtitle}</p>
+                            <p><b>Authors:</b> {item.volumeInfo.authors}</p>
+                            <p><b>Category:</b> {item.volumeInfo.categories}</p>
+                        </Card.Text>
+                        <Button variant="primary" onClick={()=> handleClick(item)}>More details</Button>
+                    </Card.Body>
+                </Card>     
+            ))
+        }
+    </>
+  )
 }
 export default Personal;
