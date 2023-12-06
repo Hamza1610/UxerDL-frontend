72ab427d7bd8c05828e4cc35f5f123560281584b
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-06 03:20:18 -0800

    disconfigure

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,15 +3,18 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
+import Books from "../../apis/Books";
 import { useState } from 'react';
 
 const SearchBar = (props) => {
 
     const [searchText, setSearchText] = useState('')
+    const [booksContent, setBooksContent] = useState([])
+    const BookClass = new Books();
 
-    const handleSubmit = () => {
-
-        console.log('Submit');
+    const handleSubmit = async () => {
+        const data = await  BookClass.get_book(searchText)
+        setBooksContent(data)
     }
     return (
     <Container className='mb-2'>
@@ -21,7 +24,7 @@ const SearchBar = (props) => {
                 <Form.Control
                     type='search'
                     className="w-100"
-                    placeholder={props.hint}
+                    placeholder='Search book'
                     onChange={(e)=> setSearchText(e.target.value)}
                 />      
             </Col>
